755d8fc7c3acf71600cfe110c88fa1b967ef0f21
sb <2sbsbsb@gmail.com>
2015-05-02 10:03:05 -0700

    Collection enhancements removeIf

---
+++ Java8ByExamples/src/examples/CollectionEnhancements/CollectionEnhancementsUtil.java
@@ -0,0 +1,29 @@
+package examples.CollectionEnhancements;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class CollectionEnhancementsUtil {
+
+	public CollectionEnhancementsUtil() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 */
+	public static void removeIf() {
+		Set<String> set = new HashSet<>();
+		set.add("A");
+		set.add("AA");
+		set.add("B");
+		System.out.println(set);
+		set.removeIf(s -> s.startsWith("A"));
+		System.out.print(set);
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		removeIf();
+	}
+
+}
